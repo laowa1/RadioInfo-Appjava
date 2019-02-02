3ba5017be42ff23fad912e3a29b058a321617831
Jakob Fridesjo <tfy17jfo@cs.umu.se>
2019-02-03 00:27:12 +0100

    Tests Added

--- src/test/java/model/ChannelInfoTest.java
+++ src/test/java/model/ChannelInfoTest.java
@@ -11,20 +11,40 @@ import static org.junit.Assert.*;
 public class ChannelInfoTest {
     private ChannelInfo channelInfo;
 
+    /**
+     * setUp
+     */
     @Before
     public void setUp() {
         channelInfo = new ChannelInfo("test");
     }
 
+    /**
+     * tests general setter/getter
+     */
     @Test
     public void testGetName() {
         assertEquals("test",channelInfo.getName());
     }
 
+    /**
+     * Tests url loading
+     * @throws MalformedURLException malformed url
+     */
     @Test
     public void testImageURL() throws MalformedURLException {
         channelInfo.setImageURL(new URL
                 ("https://static-cdn.sr.se/sida/images/4540/3634468_2048_1152.jpg?preset=api-default-square"));
         assertNotNull(channelInfo.getImageURL());
+    }
+
+    /**
+     * Tests url loading
+     * @throws MalformedURLException malformed url
+     */
+    @Test
+    public void testNullURL() throws MalformedURLException {
+        channelInfo.setImageURL(new URL(null));
+        assertNull(channelInfo.getImageURL());
     }
 }
--- src/test/java/model/ProgramInfoTest.java
+++ src/test/java/model/ProgramInfoTest.java
@@ -11,20 +11,40 @@ import static org.junit.Assert.*;
 public class ProgramInfoTest {
     private ProgramInfo programInfo;
 
+    /**
+     * setUp
+     */
     @Before
     public void setUp() {
         programInfo = new ProgramInfo("test");
     }
 
+    /**
+     * Tests general setter/getter
+     */
     @Test
     public void testGetName() {
         assertEquals("test",programInfo.getName());
     }
 
+    /**
+     * Tests url loading
+     * @throws MalformedURLException malformed url
+     */
     @Test
     public void testImageURL() throws MalformedURLException {
         programInfo.setImageURL(new URL
                 ("https://static-cdn.sr.se/sida/images/4540/3634468_2048_1152.jpg?preset=api-default-square"));
         assertNotNull(programInfo.getImageURL());
+    }
+
+    /**
+     * Tests url loading
+     * @throws MalformedURLException malformed url
+     */
+    @Test
+    public void testNullURL() throws MalformedURLException {
+        programInfo.setImageURL(new URL(null));
+        assertNull(programInfo.getImageURL());
     }
 }
